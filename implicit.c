93dac92477052f68ac970b89f2289e56d6eec44e
injamurikirthi2008-boop <injamurikirthi2008@gmail.com>
2025-10-27 18:43:21 +0530

    Add files via upload

---
+++ implicit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ int main()
+ {
+ 	int x=5,y=2;
+ 	float c,d;
+ 	c=x/y;
+ 	printf("implicit value=%f",c);
+ 	d=(float)x/y;
+ 	printf(" explicit value=%f",d);
+ 	return 0;
+ 	
+ }
+
