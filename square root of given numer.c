d6f96ff18667041b565788ce475855cf96d4da19
injamurikirthi2008-boop <injamurikirthi2008@gmail.com>
2025-10-28 13:43:13 +0530

    Add files via upload

---
+++ square root of given numer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+ int main()
+ {
+ 	float x,s;
+ 	printf("enter x value:");
+ 	scanf("%f",&x);
+ 	 s=sqrt(x);
+ 	 printf("square root=%f",s);
+ 	 return 0;
+ }
