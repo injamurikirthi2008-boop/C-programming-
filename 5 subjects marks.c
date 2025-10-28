26829abc1918faab0e1db9abdf47de44e797da09
injamurikirthi2008-boop <injamurikirthi2008@gmail.com>
2025-10-28 14:25:22 +0530

    Add files via upload

---
+++ 5 subjects marks.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ int main()
+ {
+ 	int s1,s2,s3,s4,s5,total;
+ 	float avg;
+ 	printf("enter 5 subjects values ::");
+ 	scanf("%d%d%d%d%d",&s1,&s2,&s3,&s4,&s5);
+ 	total = s1+s2+s3+s4+s5;
+ 	printf(" total =%d",total);
+ 	avg=total/5;
+ 	printf("  average =%f",avg);
+ 	return 0;
+ }
