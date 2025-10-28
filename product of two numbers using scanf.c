7cf3a6d64e7622b53b397024780a7856f09fe7e0
injamurikirthi2008-boop <injamurikirthi2008@gmail.com>
2025-10-28 11:47:31 +0530

    Add files via upload

--- product of two numbers using scanf.c
+++ product of two numbers using scanf.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
  int main()
  {
  	int x,y,z;
  	printf("enter x,y values");
  	scanf("%d%d",&x,&y);
- 	z=x*y;
- 	printf("product=%d",z);
+ 	z=x/y;
+ 	printf("div=%d",z);
  	return 0;
  }
