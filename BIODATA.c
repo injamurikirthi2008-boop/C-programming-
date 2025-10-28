8304ef41efd846c91669d21d9a4cb0e60a4ab0ad
injamurikirthi2008-boop <injamurikirthi2008@gmail.com>
2025-10-28 10:53:08 +0530

    Add files via upload

---
+++ BIODATA.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ int main()
+ {
+ 	printf("NAME : I.KEERTHI");
+ 	printf(" DOB : 08-05-2008");
+ 	printf(" PLACE : VIJAYAWADA");
+ 	printf(" FATHER NAME : YESUDANAM");
+ 	printf(" MOTHER NAME : MARIYAMMA");
+ 	return 0;
+ }
