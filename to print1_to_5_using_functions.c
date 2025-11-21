02fd210bc9da1ab87c1e686a8ff20dd9d16fc622
injamurikirthi2008-boop <injamurikirthi2008@gmail.com>
2025-11-21 17:19:48 +0530

    Add files via upload

---
+++ to print1_to_5_using_functions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ void print(int);
+  int main()
+  {
+  	int i;
+  	print(i);
+  	return 0;
+  }
+  void print(int i)
+  {
+  	for(i=1;i<=5;i++)
+  	{
+  		printf("%d",i);
+	  }
+  }
