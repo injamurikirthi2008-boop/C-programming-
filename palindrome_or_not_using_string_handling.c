12482f7db2cfc70b6fa962e959bc125b2b85b036
injamurikirthi2008-boop <injamurikirthi2008@gmail.com>
2025-11-25 16:14:18 +0530

    Add files via upload

---
+++ palindrome_or_not_using_string_handling.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+  int main()
+  {
+  	char s[20];
+  	int i,j,l,flag=0;
+  	printf("enter string name");
+  	gets(s);
+  	l=strlen(s);
+  	for(i=0,j=l-1;i<j;i++,j--)
+  	{
+	 
+  		if(s[i]!=s[j])
+  		{
+  			flag=1;
+  			break;
+	    } 
+     }
+  
+  if(flag==0)
+  printf("it is palindrome");
+  else
+  printf("not a palindrome");
+  
+  return 0;
+}
